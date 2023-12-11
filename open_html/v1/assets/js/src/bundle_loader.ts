d21cd56abd57312d6ab02c6acdb3c2c1a87c4121
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2023-12-11 20:27:01 +0100

    Complete htpkg (bundle) support (with @NprogramDev)

--- open_html/v1/assets/js/src/bundle_loader.ts
+++ open_html/v1/assets/js/src/bundle_loader.ts
@@ -3,6 +3,8 @@ import * as css_lib from "css";
 
 import { version } from "./constants";
 
+const LIMIT_OF_THE_SAME_FILE = 30;
+
 interface BundleLoadResult {
     entrypoint: string;
     allowDevtools: boolean;
@@ -60,21 +62,49 @@ class Path extends String {
         return new Path(newPathStr);
     }
 }
+interface CursedFiles {
+    [key: string]: number | undefined;
+}
+let cursedFiles: CursedFiles = {};
 const importStatement = "import";
 async function replaceJSRefs(
     txt: string,
     entries: zip.Entry[],
     path: Path
 ): Promise<string> {
+    //Jump Comments
     for (let i = 0; i < txt.length; ++i) {
-        // If txt[i] == "i" it could be import
-        console.log(txt.at(i));
-        if (txt.at(i) == "i") {
+        if (txt.at(i) == "/" && txt.at(i + 1) == "/")
+            while (i < txt.length) {
+                i++;
+                if (txt.at(i) == "\n") break;
+            }
+        if (txt.at(i) == "/" && txt.at(i + 1) == "*")
+            while (i < txt.length) {
+                i++;
+                if (txt.at(i - 1) == "*" && txt.at(i) == "/") break;
+            }
+        //Jump Strings
+        if (txt.at(i) == '"' || txt.at(i) == "'" || txt.at(i) == "`") {
+            let strEnd = "";
+            strEnd = txt.at(i)!;
+            while (i < txt.length) {
+                i++;
+                if (
+                    txt.at(i) == strEnd &&
+                    (txt.at(i - 1) == "\\" ? txt.at(i - 2) == "\\" : true)
+                )
+                    break;
+            }
+        }
+        // If txt[i] == "i" it could be import + Later @ support!
+        if (txt.at(i) == "i" /*|| txt.at(i) == "@"*/) {
             // string of length is import
             const x = txt.substring(i, importStatement.length + i);
-            console.warn(x);
             // is import
-            if (x == importStatement) {
+            if (x === importStatement || txt.at(i) == "@") {
+                let atI = i;
+                let isAt = txt.at(i) == "@";
                 // find Path String
                 let currEndChar;
                 while (i < txt.length) {
@@ -93,10 +123,23 @@ async function replaceJSRefs(
                 while (j < txt.length) {
                     j++;
                     if (
-                        currEndChar == txt.at(j) &&
-                        (txt.at(j - 1) != "\\" ? txt.at(j - 2) == "\\" : false)
+                        currEndChar === txt.at(j) &&
+                        (txt.at(j - 1) == "\\" ? txt.at(j - 2) == "\\" : true)
                     ) {
-                        console.log(txt.substring(i, j));
+                        const curl = txt.slice(i + 1, j);
+
+                        let pathFolder = path.cd("..");
+                        const nwurl = await resolveURL(
+                            curl,
+                            entries,
+                            pathFolder
+                        );
+                        if (isAt) {
+                            txt = txt.slice(0, atI) + txt.slice(atI + 1);
+                        }
+                        // Insert string at Position
+                        txt = txt.slice(0, i + 1) + nwurl + txt.slice(j);
+                        i = i + nwurl.length;
                         break;
                     }
                 }
@@ -112,7 +155,7 @@ async function replaceJSFile(
     path: Path,
     entries: zip.Entry[]
 ): Promise<Blob> {
-    const res: Blob = await new Promise((reject, resolve) => {
+    const res: Blob = await new Promise((resolve, reject) => {
         let fr = new FileReader();
         fr.onload = async (e: ProgressEvent<FileReader>) => {
             let txt = e.target!.result;
@@ -135,7 +178,7 @@ async function replaceHTMLFile(
     path: Path,
     entries: zip.Entry[]
 ): Promise<Blob> {
-    const res: Blob = await new Promise((reject, resolve) => {
+    const res: Blob = await new Promise((resolve, reject) => {
         let fr = new FileReader();
         fr.onload = async (e: ProgressEvent<FileReader>) => {
             let txt = e.target!.result;
@@ -146,6 +189,7 @@ async function replaceHTMLFile(
             resolve(rt);
         };
         fr.onerror = (e) => {
+            console.error("replaceHTMLFile", e);
             reject(new Blob([]));
         };
         fr.readAsText(blob);
@@ -157,7 +201,6 @@ let splitURL = window.location.pathname;
 if (splitURL.endsWith("/")) {
     splitURL = splitURL.substring(0, splitURL.length - 1);
 }
-console.log(splitURL);
 function resolveURL(
     txt: string,
     entries: zip.Entry[],
@@ -171,32 +214,32 @@ function resolveURL(
             txt = txtArr[1];
         }
         const filePath = path.cd(txt);
-        console.log(filePath);
+        if (cursedFiles[filePath.toString()] === undefined) {
+            cursedFiles[filePath.toString()] = 1;
+        } else {
+            const a: number = cursedFiles[filePath.toString()]!;
+            cursedFiles[filePath.toString()] = a + 1;
+            if (a >= LIMIT_OF_THE_SAME_FILE) {
+                console.log("Usage of Files: ", cursedFiles);
+                alert(
+                    `You have used to many recursions with file: ${filePath.toString()}!\n Please contact the package creator!`
+                );
+                location.reload();
+            }
+        }
         let file = entries.find((a) => {
             const b = "/" + a.filename;
             return b == filePath.toString();
         });
         let currBlob = await file!.getData!(new zip.BlobWriter());
-        if (
-            filePath.endsWith(".js") /* ||
-            filePath.endsWith(".ts") ||
-            filePath.endsWith(".jsx") ||
-            filePath.endsWith(".tsx") */
-        ) {
+        if (filePath.endsWith(".js")) {
             currBlob = await replaceJSFile(currBlob, filePath, entries);
         } else if (filePath.endsWith(".css")) {
-            console.log("CSS");
             currBlob = await parse_css(currBlob, filePath, entries);
         } else if (filePath.endsWith(".html")) {
             currBlob = await replaceHTMLFile(currBlob, filePath, entries);
         }
-        return URL.createObjectURL(currBlob);
-        /*let fr = new FileReader();
-        fr.onload = async (e: ProgressEvent<FileReader>) => {
-            resolve(e.target!.result as string);
-        };
-        fr.onerror = reject;
-        fr.readAsText(currBlob);*/
+        resolve(URL.createObjectURL(currBlob));
     });
 }
 async function replaceRefs(
@@ -222,12 +265,14 @@ async function replaceRefs(
     }
 
     const serializer = new XMLSerializer();
-    return serializer.serializeToString(doc);
+    const rt = serializer.serializeToString(doc);
+    return rt;
 }
 
 export async function load_bundle(
     bundle: File
 ): Promise<BundleLoadResult | null> {
+    console.info("Loading bundle...");
     const zip_reader = new zip.ZipReader(new zip.BlobReader(bundle));
     const entries = await zip_reader.getEntries();
 
@@ -237,6 +282,7 @@ export async function load_bundle(
 
     const metaFile = entries.find((entry) => entry.filename === "meta.json")!;
     if (metaFile) {
+        console.debug("Parsing meta.json file...");
         const meta = JSON.parse(await metaFile.getData!(new zip.TextWriter()));
 
         if (meta.entrypoint) {
@@ -244,26 +290,35 @@ export async function load_bundle(
         }
 
         if (meta.allowDevtools === false) {
+            console.debug("Devtools are disabled.");
             allowDevtools = false;
         }
 
         if (meta.version === version) {
             mismatchedVersion = false;
+        } else {
+            console.debug(
+                "The bundle was built with a different version of open_html."
+            );
         }
+    } else {
+        console.debug("The bundle does not contain a meta.json file.");
     }
 
     const entrypointFile = entries.find(
         (entry) => entry.filename === entrypoint
     )!;
     if (!entrypointFile) {
-        alert("The bundle does not contain an entrypoint file.");
+        console.error("The bundle does not contain the entrypoint file.");
+        alert("The bundle does not contain the entrypoint file.");
+
         return null;
     }
     const entrypointDataUrl = await entrypointFile.getData!(
         new zip.BlobWriter("text/html")
     );
 
-    const res: Blob = await new Promise((reject, resolve) => {
+    const res: Blob = await new Promise((resolve, reject) => {
         let fr = new FileReader();
         fr.onload = async (e: ProgressEvent<FileReader>) => {
             let txt = e.target!.result;
@@ -271,14 +326,23 @@ export async function load_bundle(
             const rt = new Blob([txt as BlobPart], {
                 type: "text/html",
             });
+
+            console.debug("Bundle loaded.");
+            console.timeEnd("parseBundle");
             resolve(rt);
         };
         fr.onerror = (e) => {
+            console.error("Error parsing bundle", e);
             reject(new Blob([]));
         };
+
+        console.debug("Loading bundle...");
+        console.time("parseBundle");
         fr.readAsText(entrypointDataUrl);
     });
+
     const uri = URL.createObjectURL(res);
+
     return {
         entrypoint: uri,
         allowDevtools,
@@ -296,26 +360,31 @@ export function parse_css(
         fr.onload = async (e: ProgressEvent<FileReader>) => {
             let css_parsed = css_lib.parse(e.target!.result as string);
 
-            console.log(css_parsed);
-
             // Iterate over every url() in the CSS file and replace it with the resolved URL
             for (const rule of css_parsed.stylesheet!.rules) {
                 if (rule.type == "import") {
+                    // @ts-ignore
                     const url = rule.import!;
                     if (!url.startsWith("url(")) continue;
 
                     // Remove the url() wrapper
-                    const pure_url = url.substring(4, url.length - 1);
+                    let pure_url = url.substring(4, url.length - 1);
+                    // Remove the quotes if they exist
+                    if (pure_url.startsWith('"') || pure_url.startsWith("'")) {
+                        pure_url = pure_url.substring(1, pure_url.length - 1);
+                    }
+
                     const resolvedURL = await resolveURL(
                         pure_url,
                         entries,
                         path
                     );
-                    rule.import = resolvedURL;
+
+                    // @ts-ignore
+                    rule.import = 'url("' + resolvedURL + '")';
                 }
             }
 
-            console.error(css_lib.stringify(css_parsed));
             resolve(
                 new Blob([css_lib.stringify(css_parsed)], { type: "text/css" })
             );
